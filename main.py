784f56389fce509915c7131f61a8c90cb75e7df0
jochensteinmetz <jochen.steinmetz@web.de>
2019-01-27 21:26:35 +0100

    1st graph

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#test
+import networkx as nx
+import matplotlib.pyplot as plt
+G = nx.Graph()
+G.add_node(1)
+G.add_nodes_from([2, 3])
+G.add_edge(1, 2)
+e = (2, 3)
+G.add_edge(*e)  # unpack edge tuple*
+G = nx.petersen_graph()
+plt.subplot(121)
+nx.draw(G, with_labels=True, font_weight='bold')
+plt.subplot(122)
+nx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
+plt.show()
