23efa3489d5afe7ae36a4b1e96adfdf5ea80996b
jochensteinmetz <jochen.steinmetz@web.de>
2019-10-11 21:58:22 +0200

    1st graph

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ G.add_edge(1, 2)
 e = (2, 3)
 G.add_edge(*e)  # unpack edge tuple*
 G = nx.petersen_graph()
+#pos=nx.circular_layout(G)
 plt.subplot(121)
 nx.draw(G, with_labels=True, font_weight='bold')
 plt.subplot(122)
 nx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
 plt.show()
+
+
